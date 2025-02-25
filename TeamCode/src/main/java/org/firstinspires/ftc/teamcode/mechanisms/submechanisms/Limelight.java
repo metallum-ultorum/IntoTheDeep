2de99beb7e488a3b979cf6bd161af9452ac1a53c
Rishu Sharma <rishushar99@gmail.com>
2025-02-24 21:49:15 -0600

    finished/updated limelight submechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/submechanisms/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/submechanisms/Limelight.java
@@ -5,6 +5,12 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import org.firstinspires.ftc.teamcode.systems.DynamicInput;
 
+/**
+ * Incoming Yap Session:
+ * Limelight returns Tx and Ty values, which return angles for where a detected object is,
+ * and trig is required to get pixel or distance values.
+ * IMPORTANT: Tx and Ty are zero when no desired object is detected.
+ */
 public class Limelight {
     Limelight3A limelight;
     LLResult currentResult;
@@ -17,12 +23,19 @@ public class Limelight {
         init();
     }
 
+    /**
+     * Initializes limelight with polling rate of 100 Hz
+     */
     public void init() {
         limelight.pipelineSwitch(currentPipeline.ordinal() + 1);
         limelight.start();
         limelight.setPollRateHz(100);
     }
 
+    /**
+     * Updates the data and checks if there is a desired object detected
+     * @return whether the gamepad should vibrate based on detected object
+     */
     public boolean update() {
         currentResult = limelight.getLatestResult();
         if (currentResult.getTx() != 0 && currentResult.getTy() != 0 && lastTx != 0 && lastTy != 0) {
@@ -33,6 +46,11 @@ public class Limelight {
         return false;
     }
 
+    /**
+     * Switches the current pipeline to a new pipeline to change the color of the desired blocks
+     * @param newPipeline The new pipeline to switch to:
+     *                    YELLOW (1), RED (2), or BLUE (3)
+     */
     public void setCurrentPipeline(LimelightPipeline newPipeline) {
         currentPipeline = newPipeline;
         limelight.pipelineSwitch(currentPipeline.ordinal() + 1);
